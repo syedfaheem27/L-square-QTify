5e90f4565014560dd3bc3b6a00b6138cfe48521b
syedfaheem27 <syedfaheem2719@gmail.com>
2024-02-19 11:57:10 +0530

    Refactor Song Album

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,51 +1,13 @@
-import { useEffect, useState } from "react";
-import Card from "./components/Card/Card";
 import Hero from "./components/Hero/Hero";
 import NavBar from "./components/NavBar/NavBar";
-import axios from "axios";
-import Section from "./components/SongSection/Section";
+import Section from "./components/Album/Section";
 
 function App() {
-  const [topSongs, setTopSongs] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  //TODO: use enqueSnackbar for showing notifications
-  //and handle the isLoading states and the view corresponding to it
-  const fetchSongs = async (url) => {
-    try {
-      setIsLoading(true);
-      setError(null);
-      const data = await axios.get(url);
-      setTopSongs(data.data);
-    } catch (err) {
-      console.log(err);
-      setError(err);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchSongs("https://qtify-backend-labs.crio.do/albums/top");
-  }, []);
   return (
     <header>
       <NavBar />
       <Hero />
-
-      <Section>
-        {topSongs.map((song) => {
-          return (
-            <Card
-              key={song.id}
-              img={song.image}
-              follows={song.follows}
-              title={song.title}
-            />
-          );
-        })}
-      </Section>
+      <Section url={"https://qtify-backend-labs.crio.do/albums/top"} />
     </header>
   );
 }
---
+++ qtify/src/components/Album/Section.js
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import styles from "./Section.module.css";
+import axios from "axios";
+import Card from "../Card/Card";
+
+//TODO: make use of reusable button component
+const Section = ({ url }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const [topSongs, setTopSongs] = useState([]);
+
+  //TODO: use enqueSnackbar for showing notifications
+  //and handle the isLoading states and the view corresponding to it
+  const fetchSongs = async () => {
+    try {
+      const data = await axios.get(url);
+      setTopSongs(data.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchSongs();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <section className={styles.songs}>
+      <div className={styles.action}>
+        <span>Top Albums</span>
+        <button onClick={() => setIsOpen((o) => !o)}>
+          {isOpen ? "Collapse" : "Show all"}
+        </button>
+      </div>
+      <div className={styles["song-list"]}>
+        {isOpen &&
+          topSongs.map((song) => {
+            return (
+              <Card
+                key={song.id}
+                img={song.image}
+                follows={song.follows}
+                title={song.title}
+              />
+            );
+          })}
+        {!isOpen && <div>None</div>}
+      </div>
+    </section>
+  );
+};
+export default Section;
