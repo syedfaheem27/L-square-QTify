a65b191633f0208d87cb229ee4938d462ba0eca8
syedfaheem27 <syedfaheem2719@gmail.com>
2024-02-24 11:21:32 +0530

    Add tooltip and update card cursor settings

--- qtify/src/components/Card/Card.js
+++ qtify/src/components/Card/Card.js
@@ -2,18 +2,21 @@ import React from "react";
 
 import styles from "./Card.module.css";
 import Chip from "@mui/material/Chip";
+import Tooltip from "@mui/material/Tooltip";
 
 const Card = ({ img, follows, title }) => {
   return (
-    <div className={styles.card}>
-      <div className={styles["img-container"]}>
-        <img className={styles.img} src={img} alt={title} />
-        <div className={styles["chip-container"]}>
-          <Chip label={follows} className={styles.chip} />
+    <Tooltip title={follows} arrow className={styles.tip}>
+      <div className={styles.card}>
+        <div className={styles["img-container"]}>
+          <img className={styles.img} src={img} alt={title} />
+          <div className={styles["chip-container"]}>
+            <Chip label={follows} className={styles.chip} />
+          </div>
         </div>
+        <span className={styles.txt}>{title}</span>
       </div>
-      <span className={styles.txt}>{title}</span>
-    </div>
+    </Tooltip>
   );
 };
 
